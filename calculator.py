baac32f9bfaf64e6c4c6253a0e838e94f0647513
mariam <javaxishvilimariami88@Gmail.com>
2021-11-14 21:05:57 +0400

    added calculator function and it's unittest

--- calculator.py
+++ calculator.py
@@ -5,3 +5,12 @@ def is_even(x):
         return 'it is odd'
     else:
         return 'Number is float'
+
+
+def calculator(product_price):
+    # product price increased with 10%
+    increased_price = product_price + ((product_price / 100) * 10)
+    # after one month product price decreased with 35%
+    decreased_price = increased_price - ((increased_price / 100)*35)
+    return decreased_price
+
--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,10 @@
 import unittest
-from calculator import is_even
+from calculator import is_even, calculator
 
 
 class TestCalculator(unittest.TestCase):
     def test_is_even(self):
         self.assertEqual('it is odd', is_even(5))
 
+    def test_calculation(self):
+        self.assertEqual(250.25, calculator(350))
